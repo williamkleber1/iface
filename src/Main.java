cf19ffa9d756e24dd2aa862da552bf802949c4c1
giva.lima <glnjunior@ic.ufal.br>
2018-03-09 15:52:49 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main 
+{
+	
+	
+	public static  void logar(ArrayList<Pessoa> usuarios) 
+	{
+		Scanner ler= new Scanner(System.in);
+		System.out.println("digite seu email\n");
+		String emailLogar = ler.nextLine();
+		System.out.println("digite sua senha\n");
+		String senhaLogar = ler.nextLine();
+		Pessoa usuarioLogado = null;
+		for(int i = 0; i < usuarios.size(); i++)
+		{
+			if(usuarios.indexOf(i).senha == senhaLogar)
+			{
+				System.out.println("---------------------ok---------------");
+			}
+		}
+		
+	}
+
+	public static void criarConta(int id,ArrayList<Pessoa> usuarios) 
+	{
+		Scanner ler= new Scanner(System.in);
+		System.out.println("digite seu nome\n");
+		String novoNome = ler.nextLine();
+		System.out.println("digite seu email\n");
+		String novoEmail = ler.nextLine();
+		System.out.println("digite sua senha\n");
+		String novaSenha = ler.nextLine();
+		
+		Pessoa novaPessoa = new Pessoa(id,novoNome,novoEmail,novaSenha);
+		usuarios.add(novaPessoa);
+		System.out.println("seja bem vindo "+ novoNome +", agora vc pode logar na sua conta\n\n\n");
+		
+		
+		
+	}
+	
+	public static void main(String[] args) 
+	{	
+		Scanner ler= new Scanner(System.in);
+		ArrayList<Pessoa> usuarios = new ArrayList<Pessoa> ();
+		Pessoa pessoa1 = new Pessoa(0,"pessoa1","pessoa1@mail.com","12345678");
+		usuarios.add(pessoa1);
+
+				
+		while(true)
+		{
+			System.out.println("selecione uma das opcoes\n");
+			System.out.println("1 - para logar\n");
+			System.out.println("2 - para criar uma conta\n");
+			int opcao = ler.nextInt();
+			if(opcao == 1) 
+			{
+				criarConta(usuarios.size() , usuarios);
+			}
+			else
+			{
+				logar(usuarios);
+			}
+			
+		}	
+		
+
+		
+	}
+
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Pessoa 
+{
+	public int id;
+	public String nome;
+	public String email;
+	public String senha;
+	ArrayList<Pessoa> amigos = new ArrayList<Pessoa> ();
+	ArrayList<String> menssagens = new ArrayList<String> ();
+	
+	public Pessoa(int id,String nome,String email,String senha)
+	{
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		
+	}
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	public String getNome() 
+	{
+		return nome;
+	}
+	public void setNome(String nome) 
+	{
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) 
+	{
+		this.email = email;
+	}
+	public String getSenha() 
+	{
+		return senha;
+	}
+	public void setSenha(String senha) 
+	{
+		this.senha = senha;
+	}
+	
+	public void adicionarAmigo(Pessoa pessoa) 
+	{
+		this.amigos.add(pessoa);
+		pessoa.amigos.add(this);
+	}
+	public void menssagem(String msg, Pessoa pessoa) 
+	{
+		pessoa.menssagens.add(msg + "\n recebido de :" + this.nome +"\n" );
+	}
+	
+	public void removerAmigo(Pessoa pessoa)
+	{
+		this.amigos.remove(pessoa);
+		pessoa.amigos.remove(this);
+	}
+
+}
